9bcc89993ea292cf1f449b7e0d1dd41629b48795
Deanna Kelley <deanna.georges@gmail.com>
2020-01-21 16:26:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,8 @@ class App < Sinatra::Base
   # Code your final two routes here:
   
   get "/goodbye/:name"
-  
+    @user_name = params[:name]
+    "Goodbye #{@user_name}."
   end
   
   get "/multiply:"
